9d75826a519e3b259251af29012663d695f49a33
Dominion Gbadamosi <worshipgbadamosi@live.com>
2026-02-16 01:01:05 +0000

    add: home handler in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,12 +5,18 @@ import (
 	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "at root!")
+	fmt.Println("back home!")
+}
+
 func ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "pong!")
 	fmt.Println("received a ping request!")
 }
 
 func main() {
+	http.HandleFunc("/", home)
 	http.HandleFunc("/ping", ping)
 
 	err := http.ListenAndServe(":8080", nil)

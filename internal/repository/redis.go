bc00767c37ccc07dc1182d0a24f3d91a11271ecf
Dominion Gbadamosi <worshipgbadamosi@live.com>
2026-03-09 01:48:18 +0000

    add: finished implementation of RedisURLRepository

--- internal/repository/redis.go
+++ internal/repository/redis.go
@@ -29,8 +29,15 @@ func (r *RedisURLRepository) Save(url, code string) error {
 	if err != nil {
 		return err
 	}
-
 	if exists > 0 {
+		return errors.New("code already exists")
+	}
+
+	reverseExists, reverseErr := r.client.Exists(ctx, "reverse:"+url).Result()
+	if reverseErr != nil {
+		return reverseErr
+	}
+	if reverseExists > 0 {
 		return errors.New("URL already exists")
 	}
 
@@ -46,7 +53,84 @@ func (r *RedisURLRepository) Save(url, code string) error {
 		return err
 	}
 
-	r.client.Set(ctx, "url:"+code, data, 0)
-	r.client.Set(ctx, "reverse:"+url, code, 0)
+	if codeToDataErr := r.client.Set(ctx, "url:"+code, data, 0).Err(); codeToDataErr != nil {
+		return codeToDataErr
+	}
+	if urlToCodeErr := r.client.Set(ctx, "reverse:"+url, code, 0).Err(); urlToCodeErr != nil {
+		return urlToCodeErr
+	}
+	return nil
+}
+
+func (r *RedisURLRepository) FindByURL(url string) (string, error) {
+	ctx := context.Background()
+	code, err := r.client.Get(ctx, "reverse:"+url).Result()
+	if err == redis.Nil {
+		return "", errors.New("URL not found")
+	}
+	if err != nil {
+		return "", err
+	}
+
+	return code, nil
+}
+
+func (r *RedisURLRepository) FindByCode(code string) (string, error) {
+	ctx := context.Background()
+	data, err := r.client.Get(ctx, "url:"+code).Result()
+
+	if err == redis.Nil {
+		return "", errors.New("Code not found")
+	}
+	if err != nil {
+		return "", err
+	}
+
+	var entry model.URLEntry
+	if err := json.Unmarshal([]byte(data), &entry); err != nil {
+		return "", err
+	}
+	return entry.OriginalURL, nil
+}
+
+func (r *RedisURLRepository) FindEntryByCode(code string) (*model.URLEntry, error) {
+	ctx := context.Background()
+	data, err := r.client.Get(ctx, "url:"+code).Result()
+
+	if err == redis.Nil {
+		return nil, errors.New("Code not found")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	var entry *model.URLEntry
+	if err := json.Unmarshal([]byte(data), &entry); err != nil {
+		return nil, err
+	}
+	return entry, nil
+}
+
+func (r *RedisURLRepository) IncrementClicks(code string) error {
+	ctx := context.Background()
+	data, entryGetErr := r.client.Get(ctx, "url:"+code).Result()
+	if entryGetErr != nil {
+		return entryGetErr
+	}
+
+	var entry *model.URLEntry
+	if unmarshallErr := json.Unmarshal([]byte(data), &entry); unmarshallErr != nil {
+		return unmarshallErr
+	}
+
+	entry.Clicks++
+	marshallData, marshallErr := json.Marshal(entry)
+	if marshallErr != nil {
+		return marshallErr
+	}
+
+	if updateErr := r.client.Set(ctx, "url:"+code, marshallData, 0).Err(); updateErr != nil {
+		return updateErr
+	}
 	return nil
 }
